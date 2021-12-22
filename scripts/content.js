f2dfe944a43c99313d320a698d0a574ad6fdc7f3
Alexander Noronha <alexander@noronha.asia>
2021-12-22 21:59:58 +0800

    fixed chrome extension

--- scripts/content.js
+++ scripts/content.js
@@ -2,7 +2,6 @@ console.log("Please work");
 
 window.onload = function () {
   callRails();
-  addElement();
 }
 
 function addElement() {
@@ -21,16 +20,39 @@ function addElement() {
   document.body.insertBefore(newDiv, currentDiv[0]);
 }
 
+
 function callRails() {
   console.log("in call rails")
   const url = new URL("http://localhost:3000/lessons/1/lesson_steps")
   fetch(url, {
     method: 'GET',
-    credentials: 'include',
-    headers: { 'Accept': 'application/json'},
-    mode: "no-cors"
+    // credentials: 'include',
+    headers: { 'Accept': 'application/json'}
+    // ,mode: "no-cors"
   }
     )
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => dataProcess(data));
+}
+
+function dataProcess(data) {
+  console.log(data[0].id)
+  // const docUrl = document.url
+  data.forEach( element => addElement(element.pop_up_text))
+}
+
+function addElement(text) {
+  // create a new div element
+  console.log("in js addElement")
+  const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode(text)
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.querySelector("p");
+  document.body.insertBefore(newDiv, currentDiv[0]);
 }
