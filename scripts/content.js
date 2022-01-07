4f1b2f47e2ea5f1a2fd7d5ef0102a31d65bb4810
Alexander Noronha <alexander@noronha.asia>
2022-01-07 16:53:08 +0800

    added regex to chrome extension

--- scripts/content.js
+++ scripts/content.js
@@ -91,8 +91,8 @@ function callRails(userDetails) {
   console.log("Call Rails Started, Fetching Data Now")
   console.log("localstorage lessonid:", localStorage.getItem('lessonid'))
   if (localStorage.getItem('lessonid') !== null) {
-    // const url = new URL(`http://localhost:3000/lessons/${localStorage.getItem(`lessonid`)}/lesson_steps`)
-    const url = new URL(`https://www.univerlay.me/lessons/${localStorage.getItem(`lessonid`)}/lesson_steps`)
+    const url = new URL(`http://localhost:3000/lessons/${localStorage.getItem(`lessonid`)}/lesson_steps`)
+    // const url = new URL(`https://www.univerlay.me/lessons/${localStorage.getItem(`lessonid`)}/lesson_steps`)
     console.log("url", url)
     fetch(url, {
       method: 'GET',
@@ -107,18 +107,19 @@ function callRails(userDetails) {
 };
 
 function dataProcessURL(data, userDetails) {
-  console.log("in data process function", data)
+  console.log("in data process function");
   console.log(data);
   console.log(location.href);
   // need to use regex
-  const filteredData = data.filter(element => location.href === element.url);
+  const filteredData = data.filter(element => location.href.match(new RegExp(element.url)));
+  // const filteredData = data.filter(element => location.href === element.url);
   fetchProgress(filteredData, userDetails);
 };
 
 function fetchProgress(filteredData, userDetails) {
   console.log("Fetching progress")
-  // const url = new URL(`http://localhost:3000/lessons/${localStorage.getItem(`lessonid`)}/lesson_progresses`)
-  const url = new URL(`https://www.univerlay.me/lessons/${localStorage.getItem(`lessonid`)}/lesson_progresses`)
+  const url = new URL(`http://localhost:3000/lessons/${localStorage.getItem(`lessonid`)}/lesson_progresses`)
+  // const url = new URL(`https://www.univerlay.me/lessons/${localStorage.getItem(`lessonid`)}/lesson_progresses`)
   console.log("url", url);
   fetch(url, {
     method: 'GET',
